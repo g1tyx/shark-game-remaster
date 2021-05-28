4e420f75db2a15971580841aaf39635445d07c5a
spencers145 <smartypantsspencer@gmail.com>
2021-05-27 21:47:24 -0400

    Made a Prototype Tree
    Non-functional. Just for show.

--- js/data/aspects.js
+++ js/data/aspects.js
@@ -18,52 +18,113 @@
  */
 SharkGame.Aspects = {
     apotheosis: {
-        eventSprite: true,
-        icon: "haven-song",
-        posX: 200,
-        posY: 5,
-        width: 400,
-        height: 200,
+        posX: 380,
+        posY: 380,
+        width: 40,
+        height: 40,
 
-        max: 1,
+        max: 4,
         level: 0,
         name: "Apotheosis",
-        description: "Become unto a god.",
-        getCost(_level) {
-            return 0;
+        description: "The path begins here.",
+        getCost(level) {
+            return (level + 1) ** 2;
         },
         getEffect(level) {
-            if (level > 0) {
-                return "You've begun on your path, it is too late to look back now.";
-            }
-            return "You can do something with this essence...";
+            return "Manual resource collection x" + (level > 0 ? level * 4 : 1) + ".";
         },
-        prerequisites: ["starterResources"],
+        prerequisites: [],
         clicked(_event) {
             SharkGame.AspectTree.increaseLevel(this);
         },
         apply() {},
     },
-    starterResources: {
-        posX: 380,
-        posY: 380,
+    pathOfIndustry: {
+        posX: 490,
+        posY: 250,
         width: 40,
         height: 40,
-        level: 0,
 
         max: 5,
-        name: "Start Resources",
-        description: "Remove some of that annoying early game",
-        getCost(_level) {
-            return 0;
+        level: 0,
+        name: "Path of Industry",
+        description: "Unlock the potential of those around you.",
+        getCost(level) {
+            return level * 2 + 2;
         },
         getEffect(level) {
-            return `level: ${level}`;
+            return "Sharks catch fish " + (level > 0 ? level + 1 : 1) + " times faster.";
         },
+        prerequisites: ["apotheosis"],
         clicked(_event) {
             SharkGame.AspectTree.increaseLevel(this);
         },
-        apply(_level) {},
-        prerequisites: [],
+        apply() {},
+    },
+    pathOfEnlightenment: {
+        posX: 380,
+        posY: 250,
+        width: 40,
+        height: 40,
+
+        max: 3,
+        level: 0,
+        name: "Path of Enlightenment",
+        description: "Unlock the potential of yourself.",
+        getCost(level) {
+            return (level + 1) ** 2;
+        },
+        getEffect(level) {
+            return "Gain " + level + " extra choice" + (level > 1 ? "s" : "") + " when choosing a world to visit.";
+        },
+        prerequisites: ["apotheosis"],
+        clicked(_event) {
+            SharkGame.AspectTree.increaseLevel(this);
+        },
+        apply() {},
+    },
+    patience: {
+        posX: 380,
+        posY: 150,
+        width: 40,
+        height: 40,
+
+        max: 3,
+        level: 0,
+        name: "Patience",
+        description: "They say that good things come to those who wait.",
+        getCost(level) {
+            return (level + 2) ** 2;
+        },
+        getEffect(level) {
+            return "Gain nothing immediately. After beating 3 more worlds, gain " + 2 * (level + 1) ** 2 + " essence.";
+        },
+        prerequisites: ["pathOfEnlightenment"],
+        clicked(_event) {
+            SharkGame.AspectTree.increaseLevel(this);
+        },
+        apply() {},
+    },
+    pathOfTime: {
+        posX: 270,
+        posY: 250,
+        width: 40,
+        height: 40,
+
+        max: 5,
+        level: 0,
+        name: "Path of Time",
+        description: "Begin each journey with swiftness.",
+        getCost(level) {
+            return (level + 1) ** 2 + 1;
+        },
+        getEffect(level) {
+            return "Bring " + 10 * level ** 3 + " crabs with you between worlds.";
+        },
+        prerequisites: ["apotheosis"],
+        clicked(_event) {
+            SharkGame.AspectTree.increaseLevel(this);
+        },
+        apply() {},
     },
 };
