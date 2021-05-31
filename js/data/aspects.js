cdfbf9920d355169e388fa4808b1c3a4ed47962c
spencers145 <smartypantsspencer@gmail.com>
2021-05-31 16:50:17 -0400

    Added New Aspects
    Added some more to the tree. Will be able to do a demonstration of them pretty soon.

--- js/data/aspects.js
+++ js/data/aspects.js
@@ -19,7 +19,7 @@
 SharkGame.Aspects = {
     apotheosis: {
         posX: 380,
-        posY: 380,
+        posY: 550,
         width: 40,
         height: 40,
 
@@ -40,8 +40,8 @@ SharkGame.Aspects = {
         apply() {},
     },
     pathOfIndustry: {
-        posX: 490,
-        posY: 250,
+        posX: 690,
+        posY: 350,
         width: 40,
         height: 40,
 
@@ -63,19 +63,29 @@ SharkGame.Aspects = {
     },
     pathOfEnlightenment: {
         posX: 380,
-        posY: 250,
+        posY: 350,
         width: 40,
         height: 40,
 
-        max: 3,
+        max: 4,
         level: 0,
         name: "Path of Enlightenment",
         description: "Unlock the potential of yourself.",
         getCost(level) {
-            return (level + 1) ** 2;
+            return (level + 1) ** 2 + 1;
         },
         getEffect(level) {
-            return "Gain " + level + " extra choice" + (level > 1 ? "s" : "") + " when choosing a world to visit.";
+            //return "Gain " + level + " extra choice" + (level > 1 ? "s" : "") + " when choosing a world to visit.";
+            switch (level) {
+                case 1:
+                    return "Reveals basic information about a world before you choose to visit it.";
+                case 2:
+                    return "Reveals all properties of a world before you choose to visit it.";
+                case 3:
+                    return "Reveals important resources that are and are not present, as well as all properties of a world before you choose to visit it.";
+                case 4:
+                    return "Reveals all information about a world before you choose to visit it and gives insight into your hypothetical experience.";
+            }
         },
         prerequisites: ["apotheosis"],
         clicked(_event) {
@@ -84,8 +94,8 @@ SharkGame.Aspects = {
         apply() {},
     },
     patience: {
-        posX: 380,
-        posY: 150,
+        posX: 300,
+        posY: 250,
         width: 40,
         height: 40,
 
@@ -106,8 +116,8 @@ SharkGame.Aspects = {
         apply() {},
     },
     pathOfTime: {
-        posX: 270,
-        posY: 250,
+        posX: 70,
+        posY: 350,
         width: 40,
         height: 40,
 
@@ -122,6 +132,77 @@ SharkGame.Aspects = {
             return "Bring " + 10 * level ** 3 + " crabs with you between worlds.";
         },
         prerequisites: ["apotheosis"],
+        clicked(_event) {
+            SharkGame.AspectTree.increaseLevel(this);
+        },
+        apply() {},
+    },
+    adjustedAquadynamics: {
+        posX: 610,
+        posY: 250,
+        width: 40,
+        height: 40,
+
+        max: 4,
+        level: 0,
+        name: "Adjusted Aquadynamics",
+        description: "A thin layer of essence greatly reduces drag, improving hunting profiency.",
+        getCost(level) {
+            return 2 * level + 1;
+        },
+        getEffect(level) {
+            return "Rays hunt fish " + 2 ** level * 2.5 + "x faster.";
+        },
+        prerequisites: ["pathOfIndustry"],
+        clicked(_event) {
+            SharkGame.AspectTree.increaseLevel(this);
+        },
+        apply() {},
+    },
+    destinyGamble: {
+        posX: 460,
+        posY: 250,
+        width: 40,
+        height: 40,
+
+        max: 5,
+        level: 0,
+        name: "Destiny Gamble",
+        description: "Where we end up is all luck, but sometimes, we can stack the deck.",
+        getCost(level) {
+            switch (level) {
+                case 0:
+                    return 1;
+                default:
+                    return 3 * level;
+            }
+        },
+        getEffect(level) {
+            return "Between worlds, have the opportunity to reroll your world selection up to " + level + " time" + (level > 0 ? "s" : "") + ".";
+        },
+        prerequisites: ["pathOfEnlightenment"],
+        clicked(_event) {
+            SharkGame.AspectTree.increaseLevel(this);
+        },
+        apply() {},
+    },
+    syntheticTransmutation: {
+        posX: 770,
+        posY: 250,
+        width: 40,
+        height: 40,
+
+        max: 4,
+        level: 0,
+        name: "Synthetic Transmutation",
+        description: "Sharkonium is naturally unaffected by small impurities. Perhaps we can cheat a little.",
+        getCost(level) {
+            return 2 * level + 3;
+        },
+        getEffect(level) {
+            return "Sharkonium is " + 20 * level + "% cheaper to produce.";
+        },
+        prerequisites: ["pathOfIndustry"],
         clicked(_event) {
             SharkGame.AspectTree.increaseLevel(this);
         },
