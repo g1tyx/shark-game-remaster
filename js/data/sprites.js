821ca1a10fbcdfa01cc7ce968239909e5f5e4582
spencers145 <smartypantsspencer@gmail.com>
2021-07-05 18:35:36 -0400

    Image Stuff, Added Squid Sprite
    Added squid sprite. Moved images around and copied them and stuff.

--- js/data/sprites.js
+++ js/data/sprites.js
@@ -1303,6 +1303,14 @@ SharkGame.Sprites = {
         sourceSize: { w: 50, h: 50 },
         pivot: { x: 0, y: 0 },
     },
+    "actions/getSquid": {
+        frame: { x: 2282, y: 50, w: 50, h: 50 },
+        rotated: false,
+        trimmed: false,
+        spriteSourceSize: { x: 0, y: 0, w: 50, h: 50 },
+        sourceSize: { w: 50, h: 50 },
+        pivot: { x: 0, y: 0 },
+    },
     "actions/getTechnician": {
         frame: { x: 2068, y: 50, w: 50, h: 50 },
         rotated: false,
