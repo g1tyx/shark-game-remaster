06ef5d3264bd9d91d9890bd53bfc3961ba30fa31
spencers145 <smartypantsspencer@gmail.com>
2021-07-09 10:58:37 -0400

    Two Sprites
    Added sprites for Crustacean Aptitude and Claw Sharpening.

--- js/data/sprites.js
+++ js/data/sprites.js
@@ -1047,22 +1047,22 @@ SharkGame.Sprites = {
         sourceSize: { w: 32, h: 32 },
         pivot: { x: 0, y: 0 },
     },
-    /* "aspects/patience": {
-        frame: { x: 2218, y: 0, w: 32, h: 32 },
+    "aspects/crustaceanAptitude": {
+        frame: { x: 2364, y: 32, w: 32, h: 32 },
         rotated: false,
         trimmed: false,
         spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 },
         sourceSize: { w: 32, h: 32 },
         pivot: { x: 0, y: 0 },
     },
-    "aspects/patience": {
-        frame: { x: 2218, y: 0, w: 32, h: 32 },
+    "aspects/clawSharpening": {
+        frame: { x: 2364, y: 64, w: 32, h: 32 },
         rotated: false,
         trimmed: false,
         spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 },
         sourceSize: { w: 32, h: 32 },
         pivot: { x: 0, y: 0 },
-    }, */
+    },
     "actions/catchFish": {
         frame: { x: 2, y: 104, w: 50, h: 50 },
         rotated: false,
