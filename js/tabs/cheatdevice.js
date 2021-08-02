648b18716284a2b189ea5775d2558f341a149472
spencers145 <smartypantsspencer@gmail.com>
2021-08-02 10:19:40 -0400

    New Buttons for Cheats
    Added new buttons to disable formatting and toggle the debug button, and changed the dice rolls to be less awkward.

--- js/tabs/cheatdevice.js
+++ js/tabs/cheatdevice.js
@@ -87,6 +87,32 @@ SharkGame.CheatsAndDebug = {
                 cad.cheaperStuffPlease();
             },
         },
+        toggleDebugButton: {
+            get name() {
+                if (SharkGame.HomeActions.getActionTable().debugbutton.unauthorized) {
+                    return "Enable debug button";
+                } else {
+                    return "Disable debug button";
+                }
+            },
+            updates: true,
+            click() {
+                cad.toggleDebugButton();
+            },
+        },
+        toggleBeautify: {
+            get name() {
+                if (cad.noNumberBeautifying) {
+                    return "Enable number formatting";
+                } else {
+                    return "Disable number formatting";
+                }
+            },
+            updates: true,
+            click() {
+                cad.toggleBeautify();
+            },
+        },
         rollDice: {
             name: "Roll the dice for wacky effects",
             click() {
@@ -233,19 +259,28 @@ SharkGame.CheatsAndDebug = {
 
     debug() {
         SharkGame.persistentFlags.debug = true;
-        SharkGame.HomeActions.getActionTable().debugbutton.unauthorized = false;
+        //
     },
 
     hideDebug() {
         SharkGame.persistentFlags.debug = false;
         SharkGame.Tabs.cheats.discovered = false;
         SharkGame.Tabs.cheats.seen = false;
-        SharkGame.HomeActions.getActionTable().debugbutton.unauthorized = true;
-        SharkGame.HomeActions.getActionTable().debugbutton.discovered = false;
+        //SharkGame.HomeActions.getActionTable().debugbutton.unauthorized = true;
+        //SharkGame.HomeActions.getActionTable().debugbutton.discovered = false;
         if (SharkGame.Tabs.current === "cheats") {
             SharkGame.Tabs.current = "home";
         }
         main.setUpTab();
+    },
+
+    toggleDebugButton() {
+        if (SharkGame.HomeActions.getActionTable().debugbutton.unauthorized) {
+            SharkGame.HomeActions.getActionTable().debugbutton.unauthorized = false;
+        } else {
+            SharkGame.HomeActions.getActionTable().debugbutton.unauthorized = true;
+            SharkGame.HomeActions.getActionTable().debugbutton.discovered = false;
+        }
     },
 
     togglePausePlease() {
@@ -367,12 +402,21 @@ SharkGame.CheatsAndDebug = {
         main.endGame();
         return "You got it, boss.";
     },
+    toggleBeautify() {
+        cad.noNumberBeautifying = !cad.noNumberBeautifying;
+    },
     rollTheDicePlease(number = Math.floor(Math.random() * 20 + 1)) {
         switch (number) {
             case 1:
                 world.forceExistence("tar");
-                res.setResource("tar", 10000);
-                res.setTotalResource("tar", 10000);
+                if (!SharkGame.ResourceMap.get("world").income) {
+                    SharkGame.ResourceMap.get("world").income = {};
+                }
+                if (!SharkGame.ResourceMap.get("world").baseIncome) {
+                    SharkGame.ResourceMap.get("world").baseIncome = {};
+                }
+                SharkGame.ResourceMap.get("world").income.tar = 1;
+                SharkGame.ResourceMap.get("world").baseIncome.tar = 1;
                 res.reconstructResourcesTable();
                 return "Rolled a one. Uh oh.";
             case 2:
@@ -384,13 +428,17 @@ SharkGame.CheatsAndDebug = {
                 return "Rolled a two. Fish make sharks slower. Sand makes rays slower. Crystal makes crabs slower. Oops.";
             case 3:
                 if (world.doesResourceExist("fish")) {
-                    SharkGame.ResourceMap.get("fish").income = {};
+                    if (!SharkGame.ResourceMap.get("fish").income) {
+                        SharkGame.ResourceMap.get("fish").income = {};
+                    }
+                    if (!SharkGame.ResourceMap.get("fish").baseIncome) {
+                        SharkGame.ResourceMap.get("fish").baseIncome = {};
+                    }
                     SharkGame.ResourceMap.get("fish").income.shark = -0.001;
                     SharkGame.ResourceMap.get("fish").income.ray = -0.001;
                     SharkGame.ResourceMap.get("fish").income.crab = -0.001;
                     SharkGame.ResourceMap.get("fish").income.whale = -0.001;
                     SharkGame.ResourceMap.get("fish").income.squid = -0.001;
-                    SharkGame.ResourceMap.get("fish").baseIncome = {};
                     SharkGame.ResourceMap.get("fish").baseIncome.shark = -0.001;
                     SharkGame.ResourceMap.get("fish").baseIncome.ray = -0.001;
                     SharkGame.ResourceMap.get("fish").baseIncome.crab = -0.001;
@@ -433,15 +481,42 @@ SharkGame.CheatsAndDebug = {
                 res.changeResource("fish", 10000000000 * Math.random() ** 3);
                 return "Rolled a nine. You eat fish hooray!";
             case 10:
-                SharkGame.ResourceMap.get("shark").income = { shark: 1 };
-                SharkGame.ResourceMap.get("shark").baseIncome = { shark: 1 };
+                if (!SharkGame.ResourceMap.get("shark").income) {
+                    SharkGame.ResourceMap.get("shark").income = {};
+                }
+                if (!SharkGame.ResourceMap.get("shark").baseIncome) {
+                    SharkGame.ResourceMap.get("shark").baseIncome = {};
+                }
+                SharkGame.ResourceMap.get("shark").income.fish = 0;
+                SharkGame.ResourceMap.get("shark").baseIncome.fish = 0;
+                SharkGame.ResourceMap.get("shark").income.shark = 0.1;
+                SharkGame.ResourceMap.get("shark").baseIncome.shark = 0.1;
+                SharkGame.ResourceMap.get("shark").income.ray = 0.05;
+                SharkGame.ResourceMap.get("shark").baseIncome.ray = 0.05;
+                SharkGame.ResourceMap.get("shark").income.crab = 0.01;
+                SharkGame.ResourceMap.get("shark").baseIncome.crab = 0.01;
                 res.reapplyModifiers("shark", "shark");
-                return "Rolled a ten. Sharks now produce themselves. And nothing else.";
+                res.reapplyModifiers("shark", "ray");
+                res.reapplyModifiers("shark", "crab");
+                return "Rolled a ten. Sharks now produce themselves. And rays. And crabs. But not fish. Not anymore.";
             case 11:
-                cad.noNumberBeautifying = true;
-                return "Rolled an eleven...wait. Oh no. OH NO. WHAT DID YOU DO?!";
+                res.addNetworkNode(SharkGame.GeneratorIncomeAffectors, "nurse", "exponentiate", "nurse", 1.01);
+                res.addNetworkNode(SharkGame.GeneratorIncomeAffectors, "nurse", "exponentiate", "shark", 0.98);
+                res.addNetworkNode(SharkGame.GeneratorIncomeAffectors, "maker", "exponentiate", "maker", 1.01);
+                res.addNetworkNode(SharkGame.GeneratorIncomeAffectors, "maker", "exponentiate", "ray", 0.98);
+                res.clearNetworks();
+                res.buildIncomeNetwork();
+                return "Rolled an eleven. Nurses speed up one another, but slow down sharks. Ditto for rays and makers.";
             case 12:
-                return "Rolled a twelve. Nothing happened.";
+                if (!SharkGame.ResourceMap.get("world").income) {
+                    SharkGame.ResourceMap.get("world").income = {};
+                }
+                if (!SharkGame.ResourceMap.get("world").baseIncome) {
+                    SharkGame.ResourceMap.get("world").baseIncome = {};
+                }
+                SharkGame.ResourceMap.get("world").income.shark = 1;
+                SharkGame.ResourceMap.get("world").baseIncome.shark = 1;
+                return "Rolled a twelve. The world now gives you free sharks. Sweet.";
             case 13:
                 if (world.doesResourceExist("fish")) {
                     res.addNetworkNode(SharkGame.GeneratorIncomeAffectors, "fish", "multiply", "shark", 0.0005);
@@ -494,6 +569,11 @@ SharkGame.CheatsAndDebug = {
                 SharkGame.ResourceMap.get("crystal").baseIncome = { sand: 1 };
                 SharkGame.ResourceMap.get("sand").income = { fish: 1 };
                 SharkGame.ResourceMap.get("sand").baseIncome = { fish: 1 };
+                if (!SharkGame.ResourceMap.get("fish").income) {
+                    SharkGame.ResourceMap.get("fish").income = {};
+                }
+                SharkGame.ResourceMap.get("fish").income.tar = 1;
+                SharkGame.ResourceMap.get("fish").baseIncome.tar = 1;
                 return "Rolled a sixteen. Crystals now produce sand. Sand produces fish. Fish still produces whatever it did before. What?";
             case 17:
                 world.forceExistence("crab");
@@ -504,19 +584,23 @@ SharkGame.CheatsAndDebug = {
                 return "Rolled a seventeen. The crabs. They're multiplying.";
             case 18:
                 if (world.doesResourceExist("fish")) {
-                    SharkGame.ResourceMap.get("fish").income = {};
+                    if (!SharkGame.ResourceMap.get("fish").income) {
+                        SharkGame.ResourceMap.get("fish").income = {};
+                    }
                     SharkGame.ResourceMap.get("fish").income.shark = 0.01;
-                    SharkGame.ResourceMap.get("fish").income.ray = 0.01;
-                    SharkGame.ResourceMap.get("fish").income.crab = 0.01;
-                    SharkGame.ResourceMap.get("fish").income.squid = 0.01;
-                    SharkGame.ResourceMap.get("fish").income.whale = 0.01;
+                    SharkGame.ResourceMap.get("fish").income.ray = 0.002;
+                    SharkGame.ResourceMap.get("fish").income.crab = 0.005;
+                    SharkGame.ResourceMap.get("fish").income.squid = 0.005;
+                    SharkGame.ResourceMap.get("fish").income.whale = 0.00001;
                     SharkGame.ResourceMap.get("fish").income.fish = -0.999;
-                    SharkGame.ResourceMap.get("fish").baseIncome = {};
+                    if (!SharkGame.ResourceMap.get("fish").baseIncome) {
+                        SharkGame.ResourceMap.get("fish").baseIncome = {};
+                    }
                     SharkGame.ResourceMap.get("fish").baseIncome.shark = 0.01;
-                    SharkGame.ResourceMap.get("fish").baseIncome.ray = 0.01;
-                    SharkGame.ResourceMap.get("fish").baseIncome.crab = 0.01;
-                    SharkGame.ResourceMap.get("fish").baseIncome.squid = 0.01;
-                    SharkGame.ResourceMap.get("fish").baseIncome.whale = 0.01;
+                    SharkGame.ResourceMap.get("fish").baseIncome.ray = 0.002;
+                    SharkGame.ResourceMap.get("fish").baseIncome.crab = 0.005;
+                    SharkGame.ResourceMap.get("fish").baseIncome.squid = 0.005;
+                    SharkGame.ResourceMap.get("fish").baseIncome.whale = 0.00001;
                     SharkGame.ResourceMap.get("fish").baseIncome.fish = -0.999;
                     return "Rolled an eighteen. Fish will now purchase frenzy members for you. Thank me later.";
                 }
