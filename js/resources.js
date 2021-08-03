b210e9ba9e6a6124ca6bbd7ce9e339a16dfe6b71
spencers145 <smartypantsspencer@gmail.com>
2021-08-03 18:06:03 -0400

    Fixed Bug with Base Incomes
    Fixed base incomes compounding upgrade multipliers.

--- js/resources.js
+++ js/resources.js
@@ -25,15 +25,14 @@ SharkGame.Resources = {
     init() {
         // set all the amounts and total amounts of resources to 0
         $.each(SharkGame.ResourceTable, (resourceId, resource) => {
-            SharkGame.ResourceMap.set(resourceId, {});
-            Object.defineProperties(SharkGame.ResourceMap.get(resourceId), Object.getOwnPropertyDescriptors(resource));
+            SharkGame.ResourceMap.set(resourceId, _.cloneDeep(resource));
         });
 
         SharkGame.ResourceMap.forEach((resource, resourceId) => {
             // create the baseIncome data
             if (resource.income) {
                 resource.baseIncome = {};
-                Object.defineProperties(resource.baseIncome, Object.getOwnPropertyDescriptors(resource.income));
+                Object.defineProperties(resource.baseIncome, Object.getOwnPropertyDescriptors(SharkGame.ResourceTable[resourceId].income));
             }
 
             // create the playerresources map
