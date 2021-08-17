0ba2d502c529f1b9f1186c613a998ac92d5fdabe
Tobias Berger <tobi.berger13@gmail.com>
2021-08-17 15:43:47 +0200

    SharkGame.PlayerResources type

--- js/lib/sharkgame.global.d.ts
+++ js/lib/sharkgame.global.d.ts
@@ -557,7 +557,6 @@ declare global {
         Main: MainModule;
         MathUtil: MathUtilModule;
         PaneHandler: PaneHandlerModule;
-        PlayerResources;
         ResourceCategories;
         ResourceIncomeAffected;
         ResourceIncomeAffectors;
@@ -637,6 +636,7 @@ declare global {
         /** Can be indexed with the name of a modifier to return the associated data in SharkGame.ModifierTypes. */
         ModifierReference: Map<ModifierName, Modifier>;
         PlayerIncomeTable: Map<ResourceName, number>;
+        PlayerResources: Map<ResourceName, { amount: number; totalAmount: number }>;
     };
 
     type SharkGame = SharkGameConstants & SharkGameUtils & SharkGameModules & SharkGameData & SharkGameRuntimeData & SharkGameTabs;
