af6badabd1adb79c916a70fc18027c1d2e0f3522
stampyzfanz <34364128+stampyzfanz@users.noreply.github.com>
2021-12-24 15:58:18 +1100

    Enabled touch events in aspect tree but no inertia
    After a movement is completed on touch, the screen should contineue to
    pan. There's probably a library that fixes it better than I did.

--- js/aspecttree.js
+++ js/aspecttree.js
@@ -335,6 +335,58 @@ SharkGame.AspectTree = {
 
         tree.context = canvas.getContext("2d", { alpha: false, desynchronized: true });
 
+        // tree doesn't work with touch screens so convert touch events to mouse events
+        // https://stackoverflow.com/questions/1517924/javascript-mapping-touch-events-to-mouse-events
+        function touchHandler(event) {
+            const touches = event.changedTouches;
+            const first = touches[0];
+            let type = "";
+            switch (event.type) {
+                case "touchstart":
+                    type = "mousedown";
+                    break;
+                case "touchmove":
+                    type = "mousemove";
+                    break;
+                case "touchend":
+                    type = "mouseup";
+                    break;
+                default:
+                    return;
+            }
+
+            // initMouseEvent(type, canBubble, cancelable, view, clickCount,
+            //                screenX, screenY, clientX, clientY, ctrlKey,
+            //                altKey, shiftKey, metaKey, button, relatedTarget);
+
+            const simulatedEvent = document.createEvent("MouseEvent");
+            simulatedEvent.initMouseEvent(
+                type,
+                true,
+                true,
+                window,
+                1,
+                first.screenX,
+                first.screenY,
+                first.clientX,
+                first.clientY,
+                false,
+                false,
+                false,
+                false,
+                0 /*left*/,
+                null
+            );
+
+            first.target.dispatchEvent(simulatedEvent);
+            event.preventDefault();
+        }
+
+        canvas.addEventListener("touchstart", touchHandler, true);
+        canvas.addEventListener("touchmove", touchHandler, true);
+        canvas.addEventListener("touchend", touchHandler, true);
+        canvas.addEventListener("touchcancel", touchHandler, true);
+
         return canvas;
     },
 
