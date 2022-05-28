89f655e075eff9b85a7429370b20368a63ae1b1d
spencers145 <smartypantsspencer@gmail.com>
2022-05-28 08:54:45 -0400

    Fixed Table Tooltip Pluralization Issue

--- js/resources.js
+++ js/resources.js
@@ -1297,19 +1297,22 @@ SharkGame.Resources = {
         let text = sharktext.getResourceName(resourceName, false, 2, sharkcolor.getElementColor("tooltipbox", "background-color"));
         if (isGeneratingText !== "") {
             text +=
-                "<br><span class='littleTooltipText'>" + sharktext.getIsOrAre(resourceName).toUpperCase() + " PRODUCING</span>" + isGeneratingText;
+                "<br><span class='littleTooltipText'>" + sharktext.getIsOrAre(resourceName, 2).toUpperCase() + " PRODUCING</span>" + isGeneratingText;
         }
         if (isConsumingText !== "") {
-            text += "<br><span class='littleTooltipText'>" + sharktext.getIsOrAre(resourceName).toUpperCase() + " CONSUMING</span>" + isConsumingText;
+            text +=
+                "<br><span class='littleTooltipText'>" + sharktext.getIsOrAre(resourceName, 2).toUpperCase() + " CONSUMING</span>" + isConsumingText;
         }
         if ((isGeneratingText || isConsumingText) && (producertext || consumertext)) {
             text += "<br><span class='littleTooltipText'>and</span>";
         }
         if (producertext !== "") {
-            text += "<br><span class='littleTooltipText'>" + sharktext.getIsOrAre(resourceName).toUpperCase() + " PRODUCED BY</span>" + producertext;
+            text +=
+                "<br><span class='littleTooltipText'>" + sharktext.getIsOrAre(resourceName, 2).toUpperCase() + " PRODUCED BY</span>" + producertext;
         }
         if (consumertext !== "") {
-            text += "<br><span class='littleTooltipText'>" + sharktext.getIsOrAre(resourceName).toUpperCase() + " CONSUMED BY</span>" + consumertext;
+            text +=
+                "<br><span class='littleTooltipText'>" + sharktext.getIsOrAre(resourceName, 2).toUpperCase() + " CONSUMED BY</span>" + consumertext;
         }
 
         if (SharkGame.ResourceMap.get(resourceName).desc) {
